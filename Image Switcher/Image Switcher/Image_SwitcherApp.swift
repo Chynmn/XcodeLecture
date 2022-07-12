5d09b1e52500a5a5cbd9ba08e6bbabad15821b54
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2022-07-12 19:05:45 +0900

    Image Switcher

---
+++ Image Switcher/Image Switcher/Image_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SwitcherApp.swift
+//  Image Switcher
+//
+//  Created by HyunMin Cho on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Image_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
