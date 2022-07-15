2b3d2e101d64478bea2eb35d0050bd28a1ac19ae
chynmn <chynmn@gmail.com>
2022-07-15 23:18:35 +0900

    ios application pratice

--- Day5/MapSample/MapSample/PoiData.swift
+++ Day5/MapSample/MapSample/PoiData.swift
@@ -21,5 +21,66 @@ class PoiItem: Codable {
 }
 
 class PoiData: ObservableObject {
-    
+    @Published var rows = [PoiItem]()
+    struct Const {
+        static let baseUrl = "https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt"
+        static let key = "e92ec3e6a9f44981974d4746d28f8995"
+    }
+    func load() {
+        let strUrl = "\(Const.baseUrl)?Type=json&Key=\(Const.key)"
+        NSLog("Loading: \(strUrl)")
+        guard let url = URL(string: strUrl) else {
+            print("Failed to build url with \(strUrl)")
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else {
+                print("No data from dataTask with url: \(strUrl)")
+                return
+            }
+            guard let pois = self.parse(data: data) else {
+                print("Parse failed")
+                return
+            }
+            OperationQueue.main.addOperation {
+                self.rows = pois
+            }
+        }
+        task.resume()
+    }
+    func parse(data: Data) -> [PoiItem]? {
+        guard let root = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            return nil
+        }
+        guard let arr = root["PlaceThatDoATasteyFoodSt"] as? [Any] else {
+            return nil
+        }
+        guard let rowobj = arr[1] as? [String: Any] else {
+            return nil
+        }
+        guard let items = rowobj["row"] as? [Any] else {
+            return nil
+        }
+        var pois = [PoiItem]()
+        for item in items {
+            guard let poi = PoiItem.from(dictionary: item) else {
+                continue
+            }
+            pois.append(poi)
+        }
+        return pois
+    }
+}
+
+fileprivate extension PoiItem {
+    static func from(dictionary: Any) -> PoiItem? {
+        guard let data = try? JSONSerialization.data(withJSONObject: dictionary) else {
+            return nil
+        }
+        let decoder = JSONDecoder()
+        guard let item = try? decoder.decode(PoiItem.self, from: data) else {
+            return nil
+        }
+        return item
+    }
 }
--- Day5/MapSample/MapSample/PoiListView.swift
+++ Day5/MapSample/MapSample/PoiListView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct PoiListView: View {
+    @ObservedObject var poiData = PoiData()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(poiData.rows, id: \.RESTRT_NM) { poi in
+                    PoiItemView(poi: poi)
+                }
+            }
+            .navigationBarTitle(Text("Restaurants"))
+        }
+        .onAppear {
+            poiData.load()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         PoiListView()
+    }
+}
+
+struct PoiItemView: View {
+    let poi: PoiItem
+    var body: some View {
+        NavigationLink {
+            PoiDetailView(poi: poi)
+        } label: {
+            HStack {
+                Image(systemName: "fork.knife.circle")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                VStack(alignment: .leading) {
+                    Text(poi.RESTRT_NM)
+                        .font(.title)
+                    Text(poi.REPRSNT_FOOD_NM)
+                        .font(.headline)
+                        .foregroundColor(.brown)
+                    Text(poi.REFINE_ROADNM_ADDR)
+                        .lineLimit(1)
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
     }
 }
