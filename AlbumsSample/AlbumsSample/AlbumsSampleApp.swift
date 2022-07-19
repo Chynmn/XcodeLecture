30ee797803e47aa9c6e8618978b5a6f5a8b3368e
HyunMin Cho <100783766+Chynmn@users.noreply.github.com>
2022-07-19 17:26:19 +0900

    Music Albums

---
+++ AlbumsSample/AlbumsSample/AlbumsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsSampleApp.swift
+//  AlbumsSample
+//
+//  Created by HyunMin Cho on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct AlbumsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
